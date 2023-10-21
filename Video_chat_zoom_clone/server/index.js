c25848bbd9c9d88d78cdf76c6b802a4db0560d5d
Anurag Singh <anurag17ai>
2023-10-22 00:47:54 +0530

    Video_chat_app

---
+++ Video_chat_zoom_clone/server/index.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const bodyParser=require('body-parser')
+const {Server}=require('socket.io');
+
+const io=new Server();
+const app=express();
+
+app.use(bodyParser.json());
+io.on("connection",(socket)=>{});
+
+
+app.listen(8000,()=>console.log('http server running at PORT 8000'));
+io.listen(8001)
